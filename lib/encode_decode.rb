8317125c1d8cf02894f8df2f6a4333993fcd0ac1
apradhn <abhijit.pradhan6@gmail.com>
2015-08-26 16:20:35 -0400

    completes encode_int64

--- lib/encode_decode.rb
+++ lib/encode_decode.rb
@@ -1,7 +1,18 @@
 def encode_int64(message)
-  # code this method first
+  h = 7
+  letters = "acdegilmnoprstuw"
+  i = 0
+  while i < message.length
+    if letters.include?(message[i])
+      h = (h * 37 + letters.index(message[i]))
+      i += 1
+    else
+      return "The word should only contain letters in acdegilmnoprstuw"
+    end
+  end
+  h
 end
 
 def decode_int64(encoded_message)
-  # code this second
+  
 end
